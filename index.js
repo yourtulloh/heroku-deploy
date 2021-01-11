53ee7330394ca0681e09753fd0c3e75c8358f392
AkhileshNS <nsakhilesh02@gmail.com>
2021-01-11 22:46:14 +0530

    Reducing overall footprint #1

--- index.js
+++ index.js
@@ -5,8 +5,6 @@ const fs = require("fs");
 const path = require("path");
 
 // Support Functions
-const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
-
 const createCatFile = ({ email, api_key }) => `cat >~/.netrc <<EOF
 machine api.heroku.com
     login ${email}
@@ -32,138 +30,27 @@ const addRemote = ({ app_name, buildpack, region, team }) => {
   }
 };
 
-const addConfig = ({ app_name, env_file, appdir }) => {
-  let configVars = [];
-  for (let key in process.env) {
-    if (key.startsWith("HD_")) {
-      configVars.push(key.substring(3) + "='" + process.env[key] + "'");
-    }
-  }
-  if (env_file) {
-    const env = fs.readFileSync(path.join(appdir, env_file), "utf8");
-    const variables = require("dotenv").parse(env);
-    const newVars = [];
-    for (let key in variables) {
-      newVars.push(key + "=" + variables[key]);
-    }
-    configVars = [...configVars, ...newVars];
-  }
-  if (configVars.length !== 0) {
-    execSync(`heroku config:set --app=${app_name} ${configVars.join(" ")}`);
-  }
-};
-
-const createProcfile = ({ procfile, appdir }) => {
-  if (procfile) {
-    fs.writeFileSync(path.join(appdir, "Procfile"), procfile);
-    execSync(`git add -A && git commit -m "Added Procfile"`);
-    console.log("Written Procfile with custom configuration");
-  }
-};
-
-const deploy = ({
-  dontuseforce,
-  app_name,
-  branch,
-  usedocker,
-  dockerHerokuProcessType,
-  dockerBuildArgs,
-  appdir,
-}) => {
-  const force = !dontuseforce ? "--force" : "";
-  if (usedocker) {
-    execSync(
-      `heroku container:push ${dockerHerokuProcessType} --app ${app_name} ${dockerBuildArgs}`,
-      appdir ? { cwd: appdir } : null
-    );
-    execSync(
-      `heroku container:release ${dockerHerokuProcessType} --app ${app_name}`,
-      appdir ? { cwd: appdir } : null
-    );
-  } else {
-    if (appdir === "") {
-      execSync(`git push heroku ${branch}:refs/heads/master ${force}`);
-    } else {
-      execSync(
-        `git push ${force} heroku \`git subtree split --prefix=${appdir} ${branch}\`:refs/heads/master`
-      );
-    }
-  }
-};
-
-const healthcheckFailed = ({
-  rollbackonhealthcheckfailed,
-  app_name,
-  appdir,
-}) => {
-  if (rollbackonhealthcheckfailed) {
-    execSync(
-      `heroku rollback --app ${app_name}`,
-      appdir ? { cwd: appdir } : null
-    );
-    core.setFailed(
-      "Health Check Failed. Error deploying Server. Deployment has been rolled back. Please check your logs on Heroku to try and diagnose the problem"
-    );
-  } else {
-    core.setFailed(
-      "Health Check Failed. Error deploying Server. Please check your logs on Heroku to try and diagnose the problem"
-    );
-  }
-};
-
 // Input Variables
 let heroku = {
   api_key: core.getInput("heroku_api_key"),
   email: core.getInput("heroku_email"),
   app_name: core.getInput("heroku_app_name"),
   buildpack: core.getInput("buildpack"),
-  branch: core.getInput("branch"),
-  dontuseforce: core.getInput("dontuseforce") === "false" ? false : true,
-  usedocker: core.getInput("usedocker") === "false" ? false : true,
-  dockerHerokuProcessType: core.getInput("docker_heroku_process_type"),
-  dockerBuildArgs: core.getInput("docker_build_args"),
-  appdir: core.getInput("appdir"),
-  healthcheck: core.getInput("healthcheck"),
-  checkstring: core.getInput("checkstring"),
-  delay: parseInt(core.getInput("delay")),
-  procfile: core.getInput("procfile"),
-  rollbackonhealthcheckfailed:
-    core.getInput("rollbackonhealthcheckfailed") === "false" ? false : true,
-  env_file: core.getInput("env_file"),
-  justlogin: core.getInput("justlogin") === "false" ? false : true,
   region: core.getInput("region"),
   team: core.getInput("team"),
 };
 
-// Formatting
-if (heroku.appdir) {
-  heroku.appdir =
-    heroku.appdir[0] === "." && heroku.appdir[1] === "/"
-      ? heroku.appdir.slice(2)
-      : heroku.appdir[0] === "/"
-      ? heroku.appdir.slice(1)
-      : heroku.appdir;
-}
-
-// Collate docker build args into arg list
-if (heroku.dockerBuildArgs) {
-  heroku.dockerBuildArgs = heroku.dockerBuildArgs
-    .split("\n")
-    .map((arg) => `${arg}="${process.env[arg]}"`)
-    .join(",");
-  heroku.dockerBuildArgs = heroku.dockerBuildArgs
-    ? `--arg ${heroku.dockerBuildArgs}`
-    : "";
-}
-
 (async () => {
   // Program logic
   try {
+    execSync(createCatFile(heroku));
+    console.log("Created and wrote to ~/.netrc");
+    execSync("heroku login");
+    console.log("Successfully logged into heroku");
+    addRemote(heroku);
+
     // Just Login
     if (heroku.justlogin) {
-      execSync(createCatFile(heroku));
-      console.log("Created and wrote to ~/.netrc");
-      execSync("heroku login");
       return;
     }
 
